1cb60006ea74a251c4462f7316a86cbf6843d984
Pierce <109320021+PierceYu@users.noreply.github.com>
2023-01-06 16:17:09 -0500

    Time: 118 ms (48.85%), Space: 42.6 MB (86.77%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -4,10 +4,8 @@
  */
 var mySqrt = function(x) {
     
-    for(let i = 0; i <= Math.round(x/2); i++) {
+    for(let i = 0; i <= x; i++) {
         if(i * i === x) return i;
         if(i * i > x) return i - 1
     }
-    
-    return 1
 };
