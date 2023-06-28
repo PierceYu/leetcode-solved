7eb9ccfa26fb6cca00ddbc30310280bf17a0d3d1
Pierce <109320021+PierceYu@users.noreply.github.com>
2023-06-28 12:15:18 -0400

    Time: 1887 ms (5.02%), Space: 74.6 MB (5.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,7 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split('').sort().join('');
-    t = t.split('').sort().join('');
-    return s === t
+   for(let i = 0; i < s.length; i++) {
+       for(let j = 0; j < t.length; j++){
+           if(t[j] === s[i]) {
+             s = s.replace(s[i],'');
+             t = t.replace(t[j],'');
+             i = 0;
+             j = 0;
+             }
+             if(s === t) return true
+       }
+   }
+    
+    return false
 };
